55f8f3ee1f7e53cda2083c0d9102d61f7bcf0297
OlhaMahdych <7866497@stud.op.edu.ua>
2025-10-20 13:32:17 +0300

    Add iterative merge sort function
    Implement iterative merge sort with comparison and assignment counters.

--- merge_sort_iterative.py
+++ merge_sort_iterative.py
@@ -1,1 +1,32 @@
-
+def merge_sort_iterative(arr):
+    n = len(arr)
+    comparisons = 0
+    assignments = 0
+    width = 1
+    while width < n:
+        for i in range(0, n, 2 * width):
+            left = arr[i:i + width]
+            right = arr[i + width:i + 2 * width]
+            merged = []
+            li = ri = 0
+            while li < len(left) and ri < len(right):
+                comparisons += 1
+                if left[li] <= right[ri]:
+                    merged.append(left[li])
+                    assignments += 1
+                    li += 1
+                else:
+                    merged.append(right[ri])
+                    assignments += 1
+                    ri += 1
+            while li < len(left):
+                merged.append(left[li])
+                assignments += 1
+                li += 1
+            while ri < len(right):
+                merged.append(right[ri])
+                assignments += 1
+                ri += 1
+            arr[i:i + len(merged)] = merged
+        width *= 2
+    return arr, comparisons, assignments
